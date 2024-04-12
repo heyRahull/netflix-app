850dac6e79e749ce78e4931b15ab05dd63e8ea91
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-04-13 00:09:15 +0530

    Api fetching & videobackground

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
 export const USER_AVATAR =
   "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzg0OThjYTQzM2YwNGMzMzFmY2VjNzc2YTM1M2RhOCIsInN1YiI6IjY2MTk1NGEyM2ZmMmRmMDE3ZDliNmRiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t1caGmmwYiVOtb-f7Knopg4Mnp8nQxDYwsR0XK5ZeUM",
+  },
+};
---
+++ src/utils/moviesSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const moviesSlice = createSlice({
+  name: "movies",
+  initialState: {
+    nowPlayingMovies: null,
+    trailerData: null,
+  },
+  reducers: {
+    addNowPlayingMovies: (state, action) => {
+      state.nowPlayingMovies = action.payload;
+    },
+    addTrailer: (state, action) => {
+      state.trailerData = action.payload;
+    },
+  },
+});
+
+export default moviesSlice.reducer;
+export const { addNowPlayingMovies, addTrailer } = moviesSlice.actions;
